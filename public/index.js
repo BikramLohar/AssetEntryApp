c998ad8e6581deb4570f1ba87286337e26d8b428
Bikram@Lohar <bikramlohar2345@gmail.com>
2025-06-12 18:02:47 +0530

    search result display on box

--- public/index.js
+++ public/index.js
@@ -1,34 +1,34 @@
 document.getElementById('assetform').addEventListener('submit',
-    function(event){
+    function (event) {
         event.preventDefault();
 
-        const formData= new FormData(event.target);
-                const data = Object.fromEntries(formData.entries());
-                fetch('http://localhost:3000/submit', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(response => response.json())
-                .then(resp => alert(resp.message))
-                .catch(error => alert('error:' + error.message))
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+        fetch('http://localhost:3000/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(resp => alert(resp.message))
+            .catch(error => alert('error:' + error.message))
     }
 
 
 )
 
 document.getElementById('file').addEventListener('change',
-    function(){
-        const file=this.files[0];
+    function () {
+        const file = this.files[0];
         console.log(file);
-        if(!file) return;
+        if (!file) return;
 
-        const allowedTypes= ['application/pdf','image/jpeg','image/jpg'];
-        if(!allowedTypes.includes(file.type)){
+        const allowedTypes = ['application/pdf', 'image/jpeg', 'image/jpg'];
+        if (!allowedTypes.includes(file.type)) {
             alert('please upload a valid file type(pdf,jpg,jpeg)')
-            this.value='';
+            this.value = '';
         }
     }
 )
@@ -46,35 +46,116 @@ document.getElementById('file').addEventListener('change',
 //     console.log(result);
 // });
 
-function searchAsset(){
- const assestCode=document.getElementById('searchAssestCode').value.trim();
+// document.addEventListener("DOMContentLoaded",()=>{
+//     const form=document.getElementById('uploadform');
 
- fetch('http://localhost:3000/search',{
-    method:'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ assetCode: assestCode })
+//     if (!form) {
+//         console.error('Form with id "uploadform" not found.');
+//         return;
+//     }
+//     form.addEventListener('submit',async function(e){
+//         e.preventDefault();
+//         const formData = new FormData(form);
 
- }).then(async response => {
-    const text = await response.text(); // safely read raw text
-    const data = text ? JSON.parse(text) : {};
-    
-    const resultDiv= document.getElementById('assetResult')
-     if (data.message) {
+//         const response = await fetch ('http://localhost:3000/submit',{
+//             method: 'POST',
+//             body: formData
+//         });
+//         const result = await response.json();
+//         alert(result.message);
+//         console.log(result);
+//     });
+// });
+
+function searchAsset() {
+    const assestCode = document.getElementById('searchAssestCode').value.trim();
+    const modal=document.getElementById('assetModal');
+    const closeBtn=document.getElementById('closebtn');
+    const resultDiv = document.getElementById('assetResult')
+
+
+    fetch('http://localhost:3000/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ assetCode: assestCode })
+
+    }).then(async response => {
+        const text = await response.text(); // safely read raw text
+        const data = text ? JSON.parse(text) : {};
+
+        
+        if (data.message) {
             resultDiv.innerHTML = `<p style="color: red;">${data.message}</p>`;
         } else {
             resultDiv.innerHTML = `
+            
                 <p><strong>Employee ID:</strong> ${data.employee_id}</p>
                 <p><strong>Current User Name:</strong> ${data.current_user_name}</p>
                 <p><strong>Employee Status:</strong> ${data.employee_status}</p>
                 <p><strong>Department:</strong> ${data.department}</p>
             `;
         }
- }).catch(error=>{
-     document.getElementById('assetResult').innerHTML = `<p style="color: red;">Search Error: ${error.message}</p>`;
- });
+        modal.style.display='block';
 
+        closeBtn.onclick=()=>{
+            modal.style.display='none';
+        }
+        window.onclick=(event)=>{
+            if(event.target==modal)
+            modal.style.display='none';
+        }
+
+
+    }).catch(error => {
+        document.getElementById('assetResult').innerHTML = `<p style="color: red;">Search Error: ${error.message}</p>`;
+    });
+
+}
+
+function verifyPasscode() {
+    const password = 1000;
+
+    const input = document.getElementById('passcodeInput').value;
+    const errmsg = document.getElementById('errorMsg');
+
+    if (Number(input) !== password) {
+        errmsg.style.display = 'block';
+    } else {
+        errmsg.style.display = 'none';
+        document.getElementById('passwordModal').style.display = 'none';
+        window.location.href = 'http://localhost:3000/download';
+    }
+    if(input === ''){
+        errmsg.style.display = 'block';
+        errmsg.textContent = 'please enter the passcode';
+    }
+}
+
+function downloadCSV() {
+    // const password=1000;
+    // const passcode=prompt('Enter the passcode to download ');
+
+    // if(Number(passcode) !== password)
+    // {
+    //     alert('Incorrect passcode .Please try again.');
+    //     return;
+    // }else{
+    //     window.location.href = 'http://localhost:3000/download';
+    // }
+
+    document.getElementById('passwordModal').style.display = 'flex';
+    document.getElementById('errorMsg').style.display = 'none';
+    document.getElementById('passcodeInput').value = '';
+    
+
+}
+
+function closeBox(){
+    document.getElementById('passwordModal').style.display='none';
+    document.getElementById('errorMsg').style.display = 'none';
+    document.getElementById('passcodeInput').value = '';
 }
 
 
